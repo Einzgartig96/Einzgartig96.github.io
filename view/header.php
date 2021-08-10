23626b36137fbb7c1fb8809ed8a5e613ec0f00d3
Einzgartig96 <teresita.guido51@gmail.com>
2021-08-09 18:49:05 -0600

    update css, user view, admin view

--- view/header.php
+++ view/header.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="img/Logo_Hotel.png" alt="logo" id="logo"></a>
+    <a class="navbar-brand" href="index.php"><img src="img/Logo_Hotel.png" alt="logo" id="logo"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon">I</span>
       </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
         <a class="nav-link" aria-current="page" href="#">  Contáctenos</a>
       </div>
       <div class="navbar-nav ml-auto" id="header-sesion">
